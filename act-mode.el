e791739d6c9eb840b40e7207c334ba23107c7d26
Lorenzo Manacorda <lorenzo@mailbox.org>
2020-02-15 20:44:58 +0100

    act-mode.el: change parent to prog-mode
    More accurate.

--- act-mode.el
+++ act-mode.el
@@ -69,7 +69,7 @@ First match should be a keyword and second an identifier."
         (modify-syntax-entry ?\n "> b" synTable)
         synTable))
 
-(define-derived-mode act-mode fundamental-mode "Act specification"
+(define-derived-mode act-mode prog-mode "Act specification"
   "act-mode is a major mode for edting .act specifications."
   (setq font-lock-defaults act-font-lock-defaults)
 
